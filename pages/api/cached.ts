d4e29c9c6599d5aaf2c9b618eb0dd6a59d8f8dbe
Ruslan Gainutdinov <contact@ruslan.org>
2022-08-12 19:32:46 +0300

    Fix display HN

--- pages/api/cached.ts
+++ pages/api/cached.ts
@@ -1,14 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import fetch from "node-fetch";
+import { request } from "@huksley/request";
 import { logger } from "../../tools/logger";
-import { takeScreenshot } from "../../src/takeScreenshot";
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   const url =
     process.env.NEXT_PUBLIC_SCREENSHOT_URL || process.env.NEXTAUTH_URL + "/api/screenshot";
 
-  const request =
+  const payload =
     req.method === "GET"
       ? {
           url: req.query.url
@@ -17,15 +16,25 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 
   logger.info("Query", url, "request", request);
 
-  const response = await fetch(url, {
+  const response = await request<{ screenshotUrl?: string }>(url, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify(request)
-  }).then(response => response.json());
+    body: JSON.stringify(payload)
+  }).then(response => response.body);
 
-  res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
+  /*
+  const response = {
+    screenshotUrl:
+      "http://puppeteerless-public.localhost:9000/puppeteerless-public/2022/1660321678646.png"
+  };*/
+
+  if (response.screenshotUrl) {
+    // Cache only if got screenshot
+    res.setHeader("Cache-Control", "s-maxage=600, stale-while-revalidate");
+  }
+
   return res.status(200).json({ ...response });
 };
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,42 +3,61 @@ import type { NextPage } from "next";
 import NextImage from "next/image";
 import { Newspaper } from "../src/newspaper";
 
-const Screenshot = ({ website }: { website: string }) => {
+const Screenshot = ({ story }: { story: Story }) => {
   const [url, setUrl] = useState<undefined | string>(undefined);
   const [refresh, setRefresh] = useState(1);
 
   useEffect(() => {
-    fetch("/api/cached?refresh=" + refresh + "&url=" + encodeURIComponent(website), {
+    fetch("/api/cached?refresh=" + refresh + "&url=" + encodeURIComponent(story.url!), {
       method: "GET"
     })
       .then(response => response.json())
       .then(response => {
         setUrl(response.screenshotUrl);
       });
-  }, [setUrl, website, refresh]);
+  }, [setUrl, story.url, refresh]);
 
   return (
-    <div
+    <span
       style={{
-        border: "5px solid black",
-        padding: "5px",
         width: "360px",
-        boxSizing: "border-box"
+        display: "flex",
+        flexDirection: "column"
       }}
     >
+      <h3
+        style={{
+          display: "block",
+          height: "3em",
+          textAlign: "center",
+          verticalAlign: "bottom"
+        }}
+      >
+        <a href={"https://news.ycombinator.com/item?id=" + story.id}>
+          <span>
+            <b>{story.score}</b>
+          </span>{" "}
+          {story.title} [{story.id}]
+        </a>
+      </h3>
+
       {url ? (
-        <img src={url} width={350} height={Math.ceil(350 * Newspaper.Berliner)} />
+        <a href={story.url}>
+          <img src={url} width={350} height={Math.ceil(350 * Newspaper.Berliner)} />
+        </a>
       ) : (
         <div
           style={{
-            width: "350px",
-            height: Math.ceil(350 * Newspaper.Berliner) + "px"
+            width: "360px",
+            padding: "10px",
+            height: Math.ceil(350 * Newspaper.Berliner) + "px",
+            backgroundColor: "#ffffff"
           }}
         >
           Fetching image...
         </div>
       )}
-      <div>
+      <div style={{ padding: "10px" }}>
         <a
           href="#"
           onClick={event => {
@@ -49,22 +68,67 @@ const Screenshot = ({ website }: { website: string }) => {
         >
           Refresh
         </a>
+        {" | "}
+        <a href={"https://news.ycombinator.com/item?id=" + story.id}>
+          Comments [{story.descendants}]
+        </a>
       </div>
-    </div>
+    </span>
   );
 };
 
+interface Story {
+  title: string;
+  id: number;
+  descendants?: number;
+  time: number;
+  url?: string;
+  score?: number;
+}
+
 const Home: NextPage = () => {
+  const [sites, setSites] = useState<Story[] | undefined>(undefined);
+  const [max, setMax] = useState(20);
+
+  useEffect(() => {
+    fetch("https://hacker-news.firebaseio.com/v0/topstories.json")
+      .then(r => r.json())
+      .then(json => {
+        Promise.all(
+          json
+            .filter((_, index) => index < max)
+            .map(id =>
+              fetch("https://hacker-news.firebaseio.com/v0/item/" + id + ".json").then(r =>
+                r.json()
+              )
+            )
+        ).then(top => setSites(top.filter(s => s.url)));
+      });
+  }, [setSites, max]);
+
   return (
     <div
       style={{
-        display: "flex"
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
       }}
     >
-      <Screenshot website="https://google.com/?q=Puppeteer" />
-      <Screenshot website="https://theguardian.com" />
-      <Screenshot website="https://techcrunch.com" />
-      <Screenshot website="https://nytimes.com" />
+      <h2>Top {sites?.length} hackernews stories</h2>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          alignItems: "center",
+          justifyContent: "center",
+          gap: "20px"
+        }}
+      >
+        {sites ? sites.map((story, index) => <Screenshot key={index} story={story} />) : null}
+      </div>
     </div>
   );
 };
